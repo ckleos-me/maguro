3bcca3f665560e7254a54626ff9323138fe3ac13
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2023-12-01 14:53:58 +0900

    all up

--- src/assets/js/include.js
+++ src/assets/js/include.js
@@ -3,4 +3,5 @@ $(function () {
     $("#footer-include").load("./parts/footer.html #footer-outer");
     $("#page-footer-include").load("../../parts/footer.html #footer-outer");
     $("#page-header-include").load("../../parts/page-header.html #header-outer");
+    $("#maguro-yukimitsu-include").load("../../menu-doc/maguroyukimitsu.html #maguroyukimitsu-outer");
 })
