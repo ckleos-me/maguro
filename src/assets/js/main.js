a47a88e96465c737f293fdc3ebfd66805ecee142
ckleo Siu <67173226+ckleo-design@users.noreply.github.com>
2023-12-08 10:51:45 +0900

    FB up

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,55 +1,49 @@
-function PageTopAnime() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 200) {
-        $('#page-top').removeClass('DownMove');
-        $('#page-top').addClass('UpMove');
-    } else {
-        if ($('#page-top').hasClass('UpMove')) {
-            $('#page-top').removeClass('UpMove');
-            $('#page-top').addClass('DownMove');
+    $('#page-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    //スクロールした際の動きを関数でまとめる
+    function PageTopAnime() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 200) { //上から200pxスクロールしたら
+            $('#page-top').addClass('show'); //#page-topについているUpMoveというクラス名を付与
+        } else {
+            $('#page-top').removeClass('show'); //UpMoveというクラス名を除き
         }
     }
-}
-
-$(window).scroll(function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
-});
-
-$(window).on('load', function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
-});
-
-$('#page-top a').click(function () {
-    $('body,html').animate({
-        scrollTop: 0 //ページトップまでスクロール
-    }, 2500); //ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false; //リンク自体の無効化
-});
-
-$(".js-support-title").click(function () {
-    $(this).next().toggle("slow");
-});
+    // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function () {
+        PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+    });
 
 
-$(".openbtn1").click(function () {
-    $(this).toggleClass('active');
-    $(".header__inner__nav").toggleClass('active');
-});
+    $(".js-support-title").click(function () {
+        $(this).next().toggle("slow");
+    });
 
-function aboutMenu() {
-    $('#about-child-menu').toggleClass('active');
-    $('#online-child-menu').removeClass('active');
-    $('#company-child-menu').removeClass('active');
-}
 
-function onlineMenu() {
-    $('#online-child-menu').toggleClass('active');
-    $('#about-child-menu').removeClass('active');
-    $('#company-child-menu').removeClass('active');
-}
+    $(".openbtn1").click(function () {
+        $(this).toggleClass('active');
+        $(".header__inner__nav").toggleClass('active');
+    });
 
-function companyMenu() {
-    $('#company-child-menu').toggleClass('active');
-    $('#about-child-menu').removeClass('active');
-    $('#online-child-menu').removeClass('active');
-}
+    function aboutMenu() {
+        $('#about-child-menu').toggleClass('active');
+        $('#online-child-menu').removeClass('active');
+        $('#company-child-menu').removeClass('active');
+    }
+
+    function onlineMenu() {
+        $('#online-child-menu').toggleClass('active');
+        $('#about-child-menu').removeClass('active');
+        $('#company-child-menu').removeClass('active');
+    }
+
+    function companyMenu() {
+        $('#company-child-menu').toggleClass('active');
+        $('#about-child-menu').removeClass('active');
+        $('#online-child-menu').removeClass('active');
+    }
